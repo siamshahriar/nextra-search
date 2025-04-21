1e25bb68416c7054c0e8c27ee605a69ae54e32fa
siamshahriar <siamshahriar99@gmail.com>
2025-04-21 16:34:57 +0600

    first commit

---
+++ src/app/_meta.js
@@ -0,0 +1,9 @@
+export default {
+  index: "Overview",
+  registration: "Registration and Account Activation",
+  project: "Project Creation",
+  web: "Web Automation",
+  api: "API Testing",
+  mobile: "Mobile Testing",
+  stress: "Stress Testing",
+};
---
+++ src/app/web/_meta.js
@@ -0,0 +1,10 @@
+export default {
+  extensionInstall: "Extension Download and Installation",
+  extWorkProcess: "Extension Work Process",
+  testCaseExecution: "Test Case Execution",
+  firstLastRun: "First and Last Run",
+  manualTestCase: "Manual Test Case",
+  schedule: "Schedule",
+  reports: "Reports",
+  ci: "CI Integration",
+};
